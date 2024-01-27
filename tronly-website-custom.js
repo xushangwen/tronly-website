c1e8cff9495fce7f7fd6de4c3d0528f03e4e1e1a
Xushangwen <147356670+xushangwen@users.noreply.github.com>
2024-01-27 15:02:34 +0800

    Update tronly-website-custom.js

--- tronly-website-custom.js
+++ tronly-website-custom.js
@@ -1,1 +1,6 @@
-
+document.getElementById('scrollToSectionHomeTest').addEventListener('click', function() {
+  const targetSection = document.querySelector('#section_home_test');
+  if (targetSection) {
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+});
