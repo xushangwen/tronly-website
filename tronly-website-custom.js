194e084e5c5de842764304cfd1287adeff530a1d
Xushangwen <147356670+xushangwen@users.noreply.github.com>
2024-01-27 16:34:04 +0800

    tronly-website-custom.js

--- tronly-website-custom.js
+++ tronly-website-custom.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     $('#scrollToSectionHomeTest').click(function() {
         $('html, body').animate({
             scrollTop: $('#section_home_test').offset().top
-        }, 6000);
+        }, 6000); // 设置动画持续时间为 1000 毫秒（1秒）
     });
 });
